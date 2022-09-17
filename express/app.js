1a5221b62a290618d38fa0b77519736f23a72e59
pam0701 <psb04027@naver.com>
2022-09-17 13:06:17 +0900

    feat: login, logout, post 오류 수정 완료

--- express/app.js
+++ express/app.js
@@ -1,16 +1,23 @@
 // @ts-check
-
 const express = require('express');
-/* const bodyParser = require('body-parser'); */
+
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const mongoClient = require('./routes/mongo');
 
 const app = express();
 const PORT = 4000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// 바디파서
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// 쿠키파서
 app.use(cookieParser());
+// 세션
 app.use(
   session({
     secret: 'klaus',
@@ -21,25 +28,57 @@ app.use(
     },
   })
 );
+// 패스포트
+app.use(passport.initialize());
+app.use(passport.session());
 
-const router = require('./routes');
-const userRouter = require('./routes/users');
-const postRouter = require('./routes/posts');
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'id',
+      passwordField: 'password',
+    },
+    async (id, password, cb) => {
+      const client = await mongoClient.connect();
+      const userCursor = client.db('people').collection('users');
+      const idResult = await userCursor.findOne({ id });
+      if (idResult !== null) {
+        if (idResult.password === password) {
+          cb(null, idResult);
+        } else {
+          cb(null, false, { message: '비밀번호가 틀렸습니다.' });
+        }
+      } else {
+        cb(null, false, { message: '해당 id가 없습니다' });
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, cb) => {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(async (id, cb) => {
+  const client = await mongoClient.connect();
+  const userCursor = client.db('people').collection('users');
+  const result = await userCursor.findOne({ id });
+  if (result !== null) cb(null, result);
+});
+
+const router = require('./routes/index');
 const boardRouter = require('./routes/board');
 const registerRouter = require('./routes/register');
 const loginRouter = require('./routes/login');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+app.use(express.static('public'));
 
 app.use('/', router);
-app.use('/users', userRouter);
-app.use('/posts', postRouter);
 app.use('/board', boardRouter);
 app.use('/register', registerRouter);
 app.use('/login', loginRouter);
-
-app.use(express.static('public'));
 
 app.use((err, req, res, next) => {
   console.log(err.stack);
--- express/routes/login.js
+++ express/routes/login.js
@@ -1,46 +1,36 @@
 // @ts-check
-
 const express = require('express');
 
 const router = express.Router();
+
+const passport = require('passport');
+
 const mongoClient = require('./mongo');
 
 router.get('/', (req, res) => {
   res.render('login');
 });
 
-router.get('/logout', async (req, res) => {
+router.post('/', async (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) throw err;
+    if (!user) {
+      return res.send(
+        `${info.message}<br><a href="/login">로그인 페이지로 이동</a>`
+      );
+    }
+    req.logIn(user, (err) => {
+      if (err) throw err;
+      res.redirect('/board');
+    });
+  })(req, res, next);
+});
+
+router.get('/logout', (req, res) => {
   req.session.destroy((err) => {
+    if (err) throw err;
     res.redirect('/');
   });
 });
 
-router.post('/', async (req, res) => {
-  const client = await mongoClient.connect();
-  const userCursor = client.db('people').collection('users');
-  const idResult = await userCursor.findOne({
-    id: req.body.id,
-  });
-  if (idResult !== null) {
-    const result = await userCursor.findOne({
-      id: req.body.id,
-      password: req.body.password,
-    });
-    if (result !== null) {
-      req.session.login = true;
-      req.session.userId = req.body.id;
-      res.redirect('/board');
-    } else {
-      res.status(404);
-      res.send(
-        '비밀번호가 틀렸습니다.<br><a href="/login">로그인 페이지로 이동</a>'
-      );
-    }
-  } else {
-    res.status(404);
-    res.send(
-      '해당 id 가 없습니다.<br><a href="/login">로그인 페이지로 이동</a>'
-    );
-  }
-});
 module.exports = router;
